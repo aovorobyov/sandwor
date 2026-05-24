34e23509d12cef4bea444c251c46d73d873e276a
Alexander Vorobyov <aovorobyov@mail.ru>
2026-05-24 20:20:34 +0300

    build errors

--- src/app/[locale]/course/[courseId]/page.tsx
+++ src/app/[locale]/course/[courseId]/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
-import { getTranslations } from 'next-intl/server';
 import { CoursePage } from '@/views/CoursePage';
 import { COURSES_REGISTRY } from '@/views/CoursePage/config/registry';
 
